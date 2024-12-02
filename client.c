075de03445d82b5278db7c1d2a4746c5709bde29
mat <128747016+mocanumatei31@users.noreply.github.com>
2024-12-02 18:47:20 +0200

    Created initial communication protocol

--- client.c
+++ client.c
@@ -1,10 +1,53 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
 
-int main()
+/* codul de eroare returnat de anumite apeluri */
+extern int errno;
+
+#define PORT 8080
+
+int main (void)
 {
-    return 0;
+  int sd;
+  struct sockaddr_in server;
+  int nr=0;
+  char msg[1024];
+  if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+      perror ("Eroare la socket().\n");
+      return errno;
+    }
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server.sin_port = htons (PORT);
+  if (connect (sd, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1)
+    {
+      perror ("Connection Error");
+      return errno;
+    }
+  printf ("[client]Introduceti path-ul fisierului pe care doriti sa il incarcati: ");
+  fflush (stdout);
+  read (0, msg, sizeof(msg));
+  printf("[client] Se trimite fisierul %s\n",msg);
+  fflush(stdout);
+  if (write (sd,&msg,sizeof(msg)) <= 0)
+    {
+      perror ("[client] Writing Error\n");
+      return errno;
+    }
+  if (read (sd, &msg,sizeof(msg)) < 0)
+    {
+      perror ("[client]Reading Error.\n");
+      return errno;
+    }
+  printf ("[client]Mesajul primit este: %s\n", msg);
+  close (sd);
 }
--- server.c
+++ server.c
@@ -1,10 +1,90 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <pthread.h>
 
-int main()
-{
-    return 0;
-}
+#define PORT 8080
+
+extern int errno;
+
+typedef struct thData {
+    int idThread;
+    int cl;
+} thData;
+
+void *treat(void *);
+
+int main() {
+    struct sockaddr_in server;
+    struct sockaddr_in from;
+    int sd;
+    pthread_t th[100];
+    int i = 0;
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("[server]Eroare la socket().\n");
+        return errno;
+    }
+    int on = 1;
+    setsockopt(sd,SOL_SOCKET,SO_REUSEADDR, &on, sizeof(on));
+    bzero(&server, sizeof (server));
+    bzero(&from, sizeof (from));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(PORT);
+
+    if (bind(sd, (struct sockaddr *) &server, sizeof(struct sockaddr)) == -1) {
+        perror("[server]Eroare la bind().\n");
+        return errno;
+    }
+
+    if (listen(sd, 2) == -1) {
+        perror("[server]Eroare la listen().\n");
+        return errno;
+    }
+    while (1) {
+        int client;
+        int length = sizeof (from);
+        printf("[server]Asteptam la portul %d...\n",PORT);
+        fflush(stdout);
+        if ((client = accept(sd, (struct sockaddr *) &from, &length)) < 0) {
+            perror("[server]Eroare la accept().\n");
+            continue;
+        }
+        thData *td = malloc(sizeof(struct thData));
+        td->idThread = i++;
+        td->cl = client;
+
+        pthread_create(&th[i], NULL, &treat, td);
+    }
+};
+
+void *treat(void *arg) {
+    struct thData tdL;
+    tdL = *((struct thData *) arg);
+    printf("[thread]- %d - Asteptam mesajul...\n", tdL.idThread);
+    fflush(stdout);
+    pthread_detach(pthread_self());
+    char msg[1024];
+    if (read(tdL.cl, &msg, sizeof(msg)) <= 0) {
+        printf("[Thread %d]\n", tdL.idThread);
+        perror("Reading Error.\n");
+    }
+    printf("[Thread %d]Mesajul a fost receptionat...%s\n", tdL.idThread, msg);
+    printf("[Thread %d]Trimitem mesajul inapoi...%s\n", tdL.idThread, msg);
+    if (write(tdL.cl, &msg, sizeof(msg)) <= 0) {
+        printf("[Thread %d] ", tdL.idThread);
+        perror("[Thread]Eroare la write() catre client.\n");
+    }
+    else {
+        printf("[Thread %d]Mesajul a fost trasmis cu succes.\n", tdL.idThread);
+    }
+    close((intptr_t) arg);
+    return (NULL);
+};
+
